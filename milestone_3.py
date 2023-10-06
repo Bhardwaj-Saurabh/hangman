be6c146b9980aaf8d0ec4e0e64be6fadd1893013
Saurabh Bhardwaj <aryan.saurabhbhardwaj@gmail.com>
2023-10-06 09:32:11 +0100

    checks if letter is acceptable

---
+++ milestone_3.py
@@ -0,0 +1,25 @@
+
+def check_guess(guess):
+    
+    # get lower case of guess
+    guess = guess.lower()
+
+    if guess in word:
+        print(f'Good guess! {guess} is in the word.')
+    else:
+        print(f'Sorry, {guess} is not in the word. Try again.')
+
+
+def ask_for_input():
+    while True:
+        # ask for a single letter from user
+        guess = input('Enter a single letter: ')
+        if len(guess) == 1 and guess.isalpha():
+            print('Good guess!')
+            break
+        else:
+            print('Invalid letter. Please, enter a single alphabetical character.')
+
+    check_guess(guess)
+
+ask_for_input()
